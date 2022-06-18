567c46254e64e6f48f83e68e6c219bca198968ae
Barakat Oseni <99281587+Barakat-Oseni@users.noreply.github.com>
2022-06-18 04:36:53 +0100

    Update urls.py

--- I4G015985PPM/I4G015985PPM/urls.py
+++ I4G015985PPM/I4G015985PPM/urls.py
@@ -1,4 +1,4 @@
-"""I4G010503MXM URL Configuration
+"""I4G015985PPM URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
