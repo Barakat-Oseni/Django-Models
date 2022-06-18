0c3c50d0dd1e8dce0acc52f26fc57429d0406686
Barakat Oseni <99281587+Barakat-Oseni@users.noreply.github.com>
2022-06-18 04:37:29 +0100

    Update wsgi.py

--- I4G015985PPM/I4G015985PPM/wsgi.py
+++ I4G015985PPM/I4G015985PPM/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for I4G010503MXM project.
+WSGI config for I4G015985PPM project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G010503MXM.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G015985PPM.settings')
 
 application = get_wsgi_application()
